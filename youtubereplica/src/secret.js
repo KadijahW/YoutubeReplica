df89a31d2143150bc173533d75e25c2438e7afb4
kadjiah <kadijahwilson@pursuit.org>
2020-07-30 14:18:30 -0400

    created file to hold API KEY

---
+++ youtubereplica/src/secret.js
@@ -0,0 +1,5 @@
+// const API_KEY = "AIzaSyC2vDVGbIsu9CQFaCBnA0-e_3c9jKSAWHU" 
+// const API_KEY = "AIzaSyBIsMnt1QmWnuzjJ5q0dvlUwgQMJGqVyoE"
+const API_KEY = "AIzaSyD8yFyUHZuxXuM5CG5gZxfAvDhCzpBDZ3A"
+
+export default API_KEY;
